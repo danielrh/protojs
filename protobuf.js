1be64d8de471b0424cc2903881662ea5df61012a
Patrick Horn <patrick.horn@gmail.com>
2009-11-01 02:33:41 -0800

    Added Base64Stream

--- protobuf.js
+++ protobuf.js
@@ -414,10 +414,20 @@ PROTO.Stream = function () {
 };
 PROTO.Stream.prototype = {
     read: function(amt) {
-        return [];
+        var result = [];
+        for (var i = 0; i < amt; ++i) {
+            var byte = this.readByte();
+            if (byte === null) {
+                break;
+            }
+            result.push(byte);
+        }
+        return result;
     },
     write: function(array) {
-        write_pos_ += array.length;
+        for (var i = 0; i < array.length; i++) {
+            this.writeByte(array[i]);
+        }
     },
     readByte: function() {
         return null;
@@ -455,6 +465,114 @@ PROTO.ByteArrayStream.prototype.writeByte = function(byte) {
 PROTO.ByteArrayStream.prototype.valid = function() {
     return this.read_pos_ < this.array_.length;
 };
+PROTO.ByteArrayStream.prototype.getArray = function() {
+    return this.array_;
+}
+
+PROTO.Base64Stream = function(b64string) {
+    this.string_ = b64string || '';
+    this.read_pos_ = 0;
+    this.read_incomplete_value_ = 0;
+    this.read_needed_bits_ = 8;
+    this.write_extra_bits_ = 0;
+    this.write_incomplete_value_ = 0;
+    this.fixString();
+}
+PROTO.Base64Stream.prototype = new PROTO.Stream();
+(function(prototype){
+    var FromB64AlphaMinus43=[
+        62,-1,-1,-1,63,52,53,54,55,56,57,58,59,60,61,
+        -1,-1,-1,-1,-1,-1,-1,
+        0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,
+        17,18,19,20,21,22,23,24,25,
+        -1,-1,-1,-1,-1,-1,
+        26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,
+        41,42,43,44,45,46,47,48,49,50,51];
+    var ToB64Alpha=[
+        'A','B','C','D','E','F','G','H','I','J','K','L','M',
+        'N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z',
+        '0','1','2','3','4','5','6','7','8','9','+','/'];
+    prototype.fixString = function() {
+        var len = this.string_.length;
+        if (this.string_[len-1]=='=') {
+            var n = 4;
+            var cutoff = 2;
+            if (this.string_[len-2]=='=') {
+                n = 2;
+                cutoff = 3;
+            }
+            this.write_extra_bits_ = n;
+            this.write_incomplete_value_ = FromB64AlphaMinus43[
+                this.string_.charCodeAt(len-3)-43];
+            this.write_incomplete_value_ >>= (6-n);
+            this.string_ = this.string_.substring(0,len-cutoff);
+        }
+    }
+    prototype.readByte = function readByte() {
+        var next6bits;
+        while (next6bits == undefined || next6bits == -1) {
+            if (this.read_pos_ >= this.string_.length) {
+                if (this.valid()) {
+                    next6bits = this.write_incomplete_value_;
+                    this.read_pos_++;
+                    break;
+                } else {
+                    return null;
+                }
+            }
+            next6bits = FromB64AlphaMinus43[
+                this.string_.charCodeAt(this.read_pos_++)-43];
+        }
+        var n = this.read_needed_bits_;
+        if (n == 8) {
+            this.read_incomplete_value_ = next6bits;
+            this.read_needed_bits_ = 2;
+            return this.readByte();
+        }
+        var ret = this.read_incomplete_value_<<n;
+        ret |= next6bits>>(6-n);
+        this.read_incomplete_value_ = next6bits&((1<<(6-n))-1);
+        this.read_needed_bits_ += 2;
+        return ret;
+    }
+
+    prototype.writeByte = function writeByte(byte) {
+        this.write_extra_bits_ += 2;
+        var n = this.write_extra_bits_;
+        this.string_ += ToB64Alpha[
+                byte>>n | this.write_incomplete_value_<<(8-n)];
+        this.write_incomplete_value_ = (byte&((1<<n)-1));
+        if (n == 6) {
+            this.string_ += ToB64Alpha[this.write_incomplete_value_];
+            this.write_extra_bits_ = 0;
+            this.write_incomplete_value_ = 0;
+        }
+        if (this.string_.length%77==76) {
+            this.string_ += "\n";
+        }
+    }
+
+    prototype.getString = function getString() {
+        var len = this.string_.length;
+        var retstr = this.string_;
+        var n = this.write_extra_bits_;
+        if (n > 0) {
+            retstr += ToB64Alpha[this.write_incomplete_value_<<(6-n)];
+            if (n==2) {
+                retstr += "==";
+            } else if (n==4) {
+                retstr += "=";
+            }
+        }
+        return retstr;
+    }
+    prototype.valid = function valid() {
+        return (this.read_pos_ < this.string_.length) ||
+               (this.read_pos_==this.string_.length && this.write_extra_bits_);
+    }
+})(PROTO.Base64Stream.prototype);
 
 PROTO.array =
     (function() {
@@ -1147,11 +1265,10 @@ PROTO.Message = function(name, properties) {
     };
     if (PROTO.DefineProperty !== undefined) {
         for (var prop in Composite.properties_) {
-            console.log("define prop "+prop);
             (function(prop){
             PROTO.DefineProperty(Composite.prototype, prop,
-                           function() { return this.GetField(prop); },
-                           function(newval) { this.SetField(prop, newval); });
+                           function GetProp() { return this.GetField(prop); },
+                           function SetProp(newval) { this.SetField(prop, newval); });
             })(prop);
         }
     }
