0d1da9e5b311367c2c208c9d4446b9e4dd63e83f
Patrick Horn <patrick.horn@gmail.com>
2010-02-03 00:57:00 -0800

    Added toString function for uuid, sha256 and time

--- pbj.js
+++ pbj.js
@@ -46,11 +46,41 @@ PBJ.normal=vectorGenerator(2,PROTO.Float);
 PBJ.quaternion=vectorGenerator(3,PROTO.Float);
 
 PBJ.duration = PROTO.sfixed64;
-PBJ.time = PROTO.fixed64;
 
-// fixme: 
-PBJ.sha256 = PROTO.bytes;
-PBJ.uuid = PROTO.bytes;
+var Fixed64BaseClass = function() {}; Fixed64BaseClass.prototype = PROTO.fixed64;
+
+PBJ.time = new Fixed64BaseClass;
+PBJ.time.toString = function(arg) {
+    var us1970Approx = arg.toNumber();
+    var ms1970 = Math.floor(us1970Approx/1000);
+    var sec1970 = Math.floor(us1970Approx/1000000);
+    var us = arg.sub(PROTO.I64.fromNumber(sec1970*1000000)).toNumber();
+    if (us < 0) { us += 1000000; }
+    return "[" + new Date(ms1970).toUTCString() + "]." +
+        (1000000+us).toString().substr(1);
+}
+
+var ByteBaseClass = function() {}; ByteBaseClass.prototype = PROTO.bytes;
+
+PBJ.sha256 = new ByteBaseClass;
+PBJ.sha256.toString = function(arg) {
+    str = '';
+    for (var i = 0; i < arg.length; i++) {
+        str += (256+arg[i]).toString(16).substr(1);
+    }
+    return str;
+}
+PBJ.uuid = new ByteBaseClass;
+PBJ.uuid.toString = function(arg) {
+    str = '';
+    for (var i = 0; i < arg.length; i++) {
+        if (i == 4 || i == 6 || i == 8 || i == 10) {
+            str += '-'
+        }
+        str += (256+arg[i]).toString(16).substr(1);;
+    }
+    return str;
+}
 
 PBJ.angle = PROTO.Float;
 
